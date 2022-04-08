d35b6abea571a25f712eb0cbab22feeafc377a61
Vanderlei Junior <vanderlei@estudante.ufscar.br>
2022-04-08 03:27:55 -0300

    [Python] Removendo restrição do CORS

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://127.0.0.1:5500'],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'])
